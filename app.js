330143fc15b80e24a23d94c224838fe86cc3c2a2
liliveth <lili.49_@outlook.com>
2023-11-08 16:54:12 -0300

    creando login

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ app.listen(3000,()=>console.log("servidor geekco levantado al puerto 3000"))
 app.get("/",function(req,res){
     res.send("hola")
 })
+
+app.get("/login",function(req,res){
+    res.sendFile(path.join(__dirname, "./views/login.html"));
+})

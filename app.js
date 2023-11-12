785c5c39eaf147d5aff5d13f67501e0c4a18a9fb
Alexander <alexitorjlanus@gmail.com>
2023-11-11 23:04:50 -0300

    cambios

--- app.js
+++ app.js
@@ -2,30 +2,30 @@ const express=require("express")
 const app=express()
 const path=require("path")
 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,"/public")))
 
 app.listen(3000,()=>console.log("servidor geekco levantado al puerto 3000"))
 
 
 app.get("/registro",function(req,res){
-    res.sendFile(path.join(__dirname+"views/register.html"))
+    res.sendFile(path.join(__dirname+"/views/register.html"))
 })
 
 
 app.get("/login",function(req,res){
-    res.sendFile(path.join(__dirname+"views/login.html"))
+    res.sendFile(path.join(__dirname+"/views/login.html"))
 })
 
 
 app.get("/detalles",function(req,res){
-    res.sendFile(path.join(__dirname+"views/productDetail.html"))
+    res.sendFile(path.join(__dirname+"/views/productDetail.html"))
 })
 
 
 app.get("/carrito",function(req,res){
-    res.sendFile(path.join(__dirname+"views/productCart.html"))
+    res.sendFile(path.join(__dirname+"/views/productCart.html"))
 })
 
 app.get("/",function(req,res){
-    res.sendFile(path.join(__dirname+"views/index.html"))
+    res.sendFile(path.join(__dirname+"/views/index.html"))
 })

60bb6af2243d8e63427e9d952e4289ddaf41d10e
AleVera96 <alejandrovera.av96@gmail.com>
2023-11-12 11:21:57 -0300

    Header en mobile First

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express=require("express")
 const app=express()
 const path=require("path")
 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,"/public")))
 
-app.listen(3000,()=>console.log("servidor geekco levantado al puerto 3000"))
+app.listen(3000,()=>console.log("servidor geekco levantado en puerto 3000"))
 
-app.get("/",function(req,res){
-    res.send("hola")
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname , "/views/index.html"))
 })

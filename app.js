7bad738ae94b4944ac0b17bf77b0a629673ea3bd
Alexander <alexitorjlanus@gmail.com>
2023-11-11 22:59:12 -0300

    levantando servidor

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ app.use(express.static(path.join(__dirname,"public")))
 
 app.listen(3000,()=>console.log("servidor geekco levantado al puerto 3000"))
 
+
+app.get("/registro",function(req,res){
+    res.sendFile(path.join(__dirname+"views/register.html"))
+})
+
+
+app.get("/login",function(req,res){
+    res.sendFile(path.join(__dirname+"views/login.html"))
+})
+
+
+app.get("/detalles",function(req,res){
+    res.sendFile(path.join(__dirname+"views/productDetail.html"))
+})
+
+
+app.get("/carrito",function(req,res){
+    res.sendFile(path.join(__dirname+"views/productCart.html"))
+})
+
 app.get("/",function(req,res){
-    res.send("hola")
+    res.sendFile(path.join(__dirname+"views/index.html"))
 })

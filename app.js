cc268989f398c7732f1f80f61300a024aa5d0f13
GabrielDko <gabriielrobless@gmail.com>
2023-11-13 12:35:23 -0300

    primer commit registro geekco

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express=require("express")
 const app=express()
 const path=require("path")
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname,"public")))
 
 app.listen(3000,()=>console.log("servidor geekco levantado al puerto 3000"))
 
-app.get("/",function(req,res){
-    res.send("hola")
+app.get("/registro",function(req,res){
+    res.sendFile(path.join(__dirname,"./views/registro.html"))
+})
+
+app.post("/registro",(req,res)=>{
+    console.log(req.body)
+    res.redirect("/");
 })

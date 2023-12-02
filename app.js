20ef8e84168849ea150fac13d9afdadfe269184b
Alexander <alexitorjlanus@gmail.com>
2023-12-02 13:40:15 -0300

    aceptando cambios

--- app.js
+++ app.js
@@ -25,11 +25,8 @@ app.get("/carrito", function (req, res) {
 app.get("/",function(req,res){
     res.sendFile(path.join(__dirname+"/views/index.html"))
 })
-<<<<<<< HEAD
-=======
 
 
 app.get("/footer",function(req,res){
     res.sendFile(path.join(__dirname+"/views/footer.html"))
 })
->>>>>>> carrito

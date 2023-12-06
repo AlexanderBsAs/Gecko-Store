495ac38797f12afc47b9d6f5a1e23a925fb2b137
liliveth <lili.49_@outlook.com>
2023-12-06 18:20:16 -0300

    agregando rama dashboard

--- Geekco-Store2.0/src/controllers/productsController.js
+++ Geekco-Store2.0/src/controllers/productsController.js
@@ -1,20 +1,23 @@
-
 const fs = require("fs");
 const path = require("path");
-const json = fs.readFileSync(path.join(__dirname,"../database/products.json"),"utf-8")
+const json = fs.readFileSync(
+  path.join(__dirname, "../database/products.json"),
+  "utf-8"
+);
 const products = JSON.parse(json);
 
-
 const productsController = {
-    carrito: (req,res)=>{
-        res.render("products/productCart")
-    },
-    productDetail: (req,res)=>{
-        const id = req.params.idProducto
-        const producto = products.find(element => element.id == id);
-        res.render("products/productDetail",{producto,products})
-    }
-}
+  carrito: (req, res) => {
+    res.render("products/productCart");
+  },
+  productDetail: (req, res) => {
+    const id = req.params.idProducto;
+    const producto = products.find((element) => element.id == id);
+    res.render("products/productDetail", { producto, products });
+  },
+  dashboard: (req, res) => {
+    res.render("products/dashboard", { title: "dashboard", products });
+  },
+};
 
 module.exports = productsController;
-
--- Geekco-Store2.0/src/routes/products.js
+++ Geekco-Store2.0/src/routes/products.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {carrito,detalles,productDetail} = require("../controllers/productsController")
+const {
+  carrito,
+  detalles,
+  productDetail,
+  dashboard,
+} = require("../controllers/productsController");
 
-router.get('/carrito',carrito );
+router.get("/carrito", carrito);
 // router.get('/detalles',detalles );
-router.get('/detalles/:idProducto', productDetail)
-
+router.get("/detalles/:idProducto", productDetail);
+router.get("/dashboard", dashboard);
 
 module.exports = router;

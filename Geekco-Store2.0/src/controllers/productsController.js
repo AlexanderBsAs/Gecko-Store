2ace55a18daf81391a324f5d665f92757bd3201f
Alexander <alexitorjlanus@gmail.com>
2023-12-20 01:28:05 -0300

    falta multer XD

--- Geekco-Store2.0/src/controllers/productsController.js
+++ Geekco-Store2.0/src/controllers/productsController.js
@@ -29,14 +29,61 @@ const productsController = {
         res.redirect("/")
     },
   dashboard: (req, res) => {
+    let json = fs.readFileSync(path.join(__dirname, "../database/products.json"), "utf-8");
+    const products = JSON.parse(json);
     res.render("products/dashboard", { title: "dashboard", products });
   },
-    formUpdate: (req,res)=>{
-      const id = req.params.idProducto
+/*     formUpdate: (req,res)=>{
+      const id = req.params.id
       const producto = products.find(element => element.id == id);
-      res.render("products/formUpdate",{producto,products})
+      res.render("products/formUpdate",{producto,products,id})
+
+}, */
+edit: (req, res) => {
+  const id = +req.params.id
+  let productos = products.find((elemento) => {
+
+    return elemento.id == id
+
+  })
+  console.log(productos)
+
+  res.render("products/formupdate", { productos, id })
 
 },
+update: (req,res)=>{
+  const { name, price, stock, discount, platform, category, description, image, installments}=req.body
+  const id= +req.params.id
+  let nuevobjeto={
+   id,
+   name,
+   price: +price,
+   stock:+stock,
+   discount:+discount,
+   platform,
+   category,
+   description,
+   image,
+   installments
+  }
+  let producto = products.map((elemento) => {
+    if (elemento.id == id) {
+      nuevobjeto.imagen = elemento.image
+
+      return nuevobjeto
+    }
+   
+  return elemento
+  }) 
+  console.log(producto)
+/*  console.log(producto) */
+/* console.log(producto) */
+  let json2=JSON.stringify(producto)
+/*     console.log(json2) */
+fs.writeFileSync(path.join(__dirname, "../database/products.json"),json2,"utf-8")  
+ console.log(req.body)
+  res.redirect("/productos/dashboard")
+}
 //     update: (req, res) => {
 //       res.render("products/update");
 // }
--- Geekco-Store2.0/src/routes/products.js
+++ Geekco-Store2.0/src/routes/products.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {carrito,productDetail,productForm,create,dashboard,formUpdate} = require("../controllers/productsController")
+const {carrito,productDetail,productForm,create,dashboard, update, edit} = require("../controllers/productsController")
 
 router.get("/carrito", carrito);
 // router.get('/detalles',detalles );
@@ -8,7 +8,8 @@ router.get('/detalles/:idProducto', productDetail)
 router.get("/productForm",productForm)
 router.post("/create",create)
 router.get("/dashboard", dashboard);
-router.get("/formUpdate/:id",formUpdate);
+router.get("/formUpdate/:id",edit);
+router.put("/formUpdate/:id", update)
 // router.put('/update', update);
 
 module.exports = router;

df28cf3989b470d936995b426195ea6cfbf3469a
Alexander <alexitorjlanus@gmail.com>
2023-12-20 16:54:56 -0300

    haciendo multer

--- Geekco-Store2.0/src/routes/products.js
+++ Geekco-Store2.0/src/routes/products.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const {carrito,productDetail,productForm,create,dashboard, update, edit} = require("../controllers/productsController")
+const multer=require("multer")
+const path=require ( "path")
+
+
+let storage= multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,path.join(__dirname, "../","../","public/images/products"))
+    },
+    filename:(req,file,cb)=>{
+        let newfile= file.originalname + "-"+ Date.now()+ path.extname(file.originalname)
+        cb(null,newfile)
+    }
+})
+
+let upload= multer({storage:storage})
 
 router.get("/carrito", carrito);
 // router.get('/detalles',detalles );
@@ -9,7 +24,7 @@ router.get("/productForm",productForm)
 router.post("/create",create)
 router.get("/dashboard", dashboard);
 router.get("/formUpdate/:id",edit);
-router.put("/formUpdate/:id", update)
+router.put("/formUpdate/:id", upload.single("image"), update)
 // router.put('/update', update);
 
 module.exports = router;

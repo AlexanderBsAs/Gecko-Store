a83704ad97d4ab47e006c487ab0c1be44c43cf85
GabrielDko <gabriielrobless@gmail.com>
2023-12-21 03:05:28 -0300

    logic $ styles no responsive

--- Geekco-Store2.0/src/controllers/indexController.js
+++ Geekco-Store2.0/src/controllers/indexController.js
@@ -1,4 +1,5 @@
 
+const { log } = require("console");
 const fs = require("fs");
 const path = require("path");
 const json = fs.readFileSync(path.join(__dirname,"../database/products.json"),"utf-8")
@@ -10,12 +11,13 @@ const indexController = {
     },
     search: (req,res)=>{
         let {keywords} = req.query;
-        let search = [];
+        const search = [];
         products.forEach(product => {
-            if(product.name.toLowerCase() == keywords.toLowerCase()){
+            if(product.name.toLowerCase().includes(keywords.toLowerCase())){
                 search.push(product)
             }
         });
+        console.log(search);
         res.render("results",{search,keywords})
     }
 }

a4d973c200acdbfc4394f5627239a23e168ec5f6
FerOrtiz97 <tino.pipe97@gmail.com>
2024-01-20 18:32:11 -0300

    se creo la logica para que el usuario se pueda registrar

--- Geekco-Store2.0/src/controllers/usersController.js
+++ Geekco-Store2.0/src/controllers/usersController.js
@@ -1,22 +1,25 @@
 const fs = require("fs")
 const path = require("path")
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const usersPath = path.join(__dirname, "../database/users.json");
 
 const usersController = {
-    login: (req,res)=>{
+    login: (req, res) => {
         if (res.locals.errors) {
             res.render('users/login', { errors: res.locals.errors });
-          } else {
+        } else {
             res.render('users/login', { errors: [] });
-          }
+        }
     },
-    userLogin: (req,res)=>{
+    userLogin: (req, res) => {
         const errors = validationResult(req);
-         if (!errors.isEmpty()) { console.log(errors);
+        if (!errors.isEmpty()) {
+            console.log(errors);
             res.locals.errors = errors.array();
             return res.render('users/login', { errors: res.locals.errors });
         }
-        const json = fs.readFileSync(path.join(__dirname, "../database/users.json"),"utf-8");
+        const json = fs.readFileSync(usersPath, "utf-8");
         const users = JSON.parse(json);
         const { email, password, remember } = req.body;
         const userLogged = users.find(user => user.email === email && user.password === password);
@@ -33,12 +36,32 @@ const usersController = {
         console.log('Usuario autenticado:', userLogged);
         res.redirect('/');
     },
-    register: (req,res)=>{
+    register: (req, res) => {
         res.render("users/register")
     },
-    userRegister: (req,res)=>{
-        console.log(req.body)
-        res.redirect("/")
+    userRegister: (req, res) => {
+        const errores = validationResult(req);
+        if (!errores.isEmpty()) {
+            console.log("Errores:", errores);
+            return res.render('./users/register', { errores: errores.mapped(), old: req.body, title: "registro" });
+        }
+        const { first_name, last_name, email, password, adress } = req.body;
+        const id = Date.now().toString();
+        const newUser = {
+            id,
+            first_name: first_name.trim(),
+            last_name: last_name.trim(),
+            email: email.trim(),
+            password: bcrypt.hashSync(password, 10),
+            adress: adress.trim(),
+            admin: false,
+            image: req.file ? req.file.filename : "default.jpg",
+        };
+        const json = fs.readFileSync(usersPath, "utf-8");
+        const users = JSON.parse(json);
+        users.push(newUser);
+        fs.writeFileSync(usersPath, JSON.stringify(users), "utf-8");
+        res.redirect('/users/login');
     }
 }
 

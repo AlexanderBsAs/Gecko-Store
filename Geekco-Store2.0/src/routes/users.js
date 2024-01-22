ad768d59c13831d4ce40b86bf9ac0fe2a607faf1
AleVera96 <alejandrovera.av96@gmail.com>
2024-01-22 15:06:00 -0300

    Refactoring code

--- Geekco-Store2.0/src/routes/users.js
+++ Geekco-Store2.0/src/routes/users.js
@@ -5,23 +5,14 @@ const {login,userLogin,register,userRegister,logout} = require("../controllers/u
 const guestMiddleware = require('../Middlewares/guestMiddleware')
 const userAuthMiddleware = require('../Middlewares/userAuthMiddleware')
 const {validacionRegistro} = require("../validations/registerValidator")
-const multer=require("multer")
-const path=require("path")
 const upload = require('../validations/uploadUser');
 
 /* GET users listing. */
-router.get('/login',login );
-router.post('/login',loginValidationRules, userLogin );
 router.get('/login',guestMiddleware,login );
-
-
-
-
+router.post('/login',loginValidationRules, userLogin );
 router.get('/registro',guestMiddleware,register );
 router.post('/registro',upload.single('image'),validacionRegistro,userRegister );
-
 // Ruta de logout del usuario
-
 router.get('/logout', logout)
 
 

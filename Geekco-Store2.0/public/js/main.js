e1d081477d5c6b1d1b0cfa4000982cae65cca5c7
AleVera96 <alejandrovera.av96@gmail.com>
2024-04-04 20:35:13 -0300

    Refactoring views

--- Geekco-Store2.0/public/js/main.js
+++ Geekco-Store2.0/public/js/main.js
@@ -1,15 +1,24 @@
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const abrirlog = document.querySelector("#abrir__logeo1");
-const navlog = document.querySelector(".header__nav1");
+window.addEventListener('load', function() {
+    const nav = document.querySelector("#nav");
+    const abrir = document.querySelector("#abrir");
+    const abrirlog = document.querySelector("#abrir__logeo1");
+    const navlog = document.querySelector(".header__nav1");
 
+    abrirlog.addEventListener("click", () => {
+        if (navlog.style.display === "none" || navlog.style.visibility === "hidden") {
+            navlog.style.display = "block";
+            navlog.style.visibility = "visible";
+        } else {
+            navlog.style.display = "none";
+            navlog.style.visibility = "hidden";
+        }
+    });
 
-
-abrir.addEventListener("click", () => {
-    if (nav.style.visibility === "hidden") {
-        nav.style.visibility = "visible";
-    } else {
-        nav.style.visibility = "hidden";
-    }})
-
-
+    abrir.addEventListener("click", () => {
+        if (nav.style.visibility === "hidden") {
+            nav.style.visibility = "visible";
+        } else {
+            nav.style.visibility = "hidden";
+        }
+    });
+});

a0b0234d5a282e2337f75c282661a49b6943097d
AleVera96 <alejandrovera.av96@gmail.com>
2024-04-15 19:37:50 -0300

    Adding user session data in UpBar, updating styles

--- Geekco-Store2.0/admin/src/components/UpBar.jsx
+++ Geekco-Store2.0/admin/src/components/UpBar.jsx
@@ -1,13 +1,50 @@
-import "../styles/stylesheets/style.css"
+import React, { useState, useEffect } from 'react';
+
 const UpBar = () => {
-    return(
-        <nav className="UpBar__nav">
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchSessionInfo = async () => {
+        try {
+          const response = await fetch('http://localhost:3000/api/session/user-info', {
+            credentials: 'include',
+            method:'GET'
+          });
+          if (!response.ok) {
+            throw new Error('Failed to fetch session info');
+          }
+          const data = await response.json();
+          setUserData(data);
+          console.log('Session info:', data);
+        } catch (error) {
+          console.error('Error fetching session info:', error);
+        }
+      };
+
+    fetchSessionInfo();
+  }, []);  // Se ejecuta solo una vez al montar el componente
+
+  if (!userData) {
+    return (
+      <nav className="UpBar__nav">
         <ul>
-            <li>Volver al inicio</li>
-            <li>Usuario</li>
+          <li>Volver al inicio</li>
+          <li>Cargando...</li>
         </ul>
-        </nav>
-    )
-}
+      </nav>
+    );
+  }
+
+  const { firstName, image } = userData;  // Extraemos el nombre y la imagen del usuario
+
+  return (
+    <nav className="UpBar__nav">
+       <a href="http://localhost:3000/">Volver al inicio</a>
+      <p>{firstName}</p>
+         <div><img src={`http://localhost:3000/images/users/${image}`}/> </div>
+
+    </nav>
+  );
+};
 
 export default UpBar;

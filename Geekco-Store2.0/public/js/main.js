5e0f5609c9a78822e1ad81d5541a14ff0674d898
GabrielDko <gabriielrobless@gmail.com>
2024-04-16 20:34:56 -0300

    refactorizando tarjetas

--- Geekco-Store2.0/public/js/main.js
+++ Geekco-Store2.0/public/js/main.js
@@ -3,9 +3,8 @@ window.addEventListener('load', function() {
     const abrir = document.querySelector("#abrir");
     const abrirlog = document.querySelector("#abrir__logeo1");
     const navlog = document.querySelector(".header__nav1");
-    const principalIconsDiv = document.querySelector(".principal-icons-div");
-    const dropdownMenu = document.getElementById("dropdown-menu");
-abrirlog.style.cursor = 'pointer';
+
+    // abrirlog.style.cursor = 'pointer';
 console.log("Archivo JS vinculado con éxito")
 abrirlog.addEventListener("click", () => {
     // Alternar la clase 'visible' para mostrar u ocultar el menú
@@ -21,11 +20,4 @@ abrirlog.addEventListener("click", () => {
         }
     });
 
-        
-      
-        principalIconsDiv.addEventListener("click", function() {
-          // Mostrar u ocultar el menú al hacer clic en el contenedor de iconos
-          dropdownMenu.style.display = dropdownMenu.style.display === "none" ? "block" : "none";
-        });
-      
 });

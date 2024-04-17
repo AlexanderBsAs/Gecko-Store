b292b0dfec366e5d38d2edb6f152eef10c0ebbda
GabrielDko <gabriielrobless@gmail.com>
2024-04-17 17:55:56 -0300

    animaciones botones go top y whatsapp

--- Geekco-Store2.0/public/js/main.js
+++ Geekco-Store2.0/public/js/main.js
@@ -1,16 +1,15 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     const nav = document.querySelector("#nav");
     const abrir = document.querySelector("#abrir");
     const abrirlog = document.querySelector("#abrir__logeo1");
     const navlog = document.querySelector(".header__nav1");
 
     // abrirlog.style.cursor = 'pointer';
-console.log("Archivo JS vinculado con éxito")
-abrirlog.addEventListener("click", () => {
-    // Alternar la clase 'visible' para mostrar u ocultar el menú
-    navlog.classList.toggle('visible');
-});
-
+    console.log("Archivo JS vinculado con éxito")
+    abrirlog.addEventListener("click", () => {
+        // Alternar la clase 'visible' para mostrar u ocultar el menú
+        navlog.classList.toggle('visible');
+    });
 
     abrir.addEventListener("click", () => {
         if (nav.style.visibility === "hidden") {
@@ -21,10 +20,34 @@ abrirlog.addEventListener("click", () => {
     });
 
     function toggleRespuesta(elemento) {
-
         var respuesta = elemento.nextElementSibling;
-
-
         respuesta.classList.toggle("visible");
     }
+    const whatsapp = document.querySelector('.icon-whatsapp-button');
+
+    whatsapp.addEventListener('click', function () {
+        const urlDestino = 'https://wa.me/1164712830';
+
+        window.open(urlDestino);
+    });
+
+
+    window.onscroll = function () {
+        if (document.documentElement.scrollTop > 1500) {
+            document.querySelector('.icon-whatsapp-container').classList.add('show')
+            document.querySelector('.go-top-container').classList.add('show')
+        } else {
+            document.querySelector('.icon-whatsapp-container').classList.remove('show')
+            document.querySelector('.go-top-container').classList.remove('show')
+        }
+
+    }
+
+    document.querySelector('.go-top-container')
+        .addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
 });
